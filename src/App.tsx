8bf2d8356c7ccef2f999a926f449759e5fdcc6af
Vy Ngo Chi <ncv.ngovy@gmail.com>
2026-03-15 16:48:01 +0700

    feat(app-config): add favicons folder, fonts, and config router for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
-import { Button } from "./components/ui/button";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
 
 function App() {
-  return (
-    <div>
-      <Button>HELLO</Button>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
